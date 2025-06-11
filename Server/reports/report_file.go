63a11fad7af1a141e7e179f33329264f5c1db610
DavidVelasquez77 <128778972+DavidVelasquez77@users.noreply.github.com>
2025-06-11 17:05:35 -0600

    report file

---
+++ Server/reports/report_file.go
@@ -0,0 +1,30 @@
+package reports
+
+import (
+	"os"
+	"server/structures"
+	"server/utils"
+)
+
+func ReportFile(sb *structures.SuperBlock, diskPath, path string, pathFileToGetInfo string) error {
+	err := utils.CreateParentDirs(path)
+	if err != nil {
+		return err
+	}
+	parentDirs, destDir := utils.GetParentDirectories(pathFileToGetInfo)
+	content, err := sb.ContentFromFile(diskPath, 0, parentDirs, destDir)
+	if err != nil {
+		return err
+	}
+	txtFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer txtFile.Close()
+	_, err = txtFile.WriteString(content)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
