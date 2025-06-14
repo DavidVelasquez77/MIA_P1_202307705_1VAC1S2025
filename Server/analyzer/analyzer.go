a9f0c7d0889dbfae7533ee7c3a8597b55b06d303
DavidVela <3626064120101@ingenieria.usac.edu.gt>
2025-06-14 12:53:43 -0600

    mejora del analyzer para ejecutar execute

--- Server/analyzer/analyzer.go
+++ Server/analyzer/analyzer.go
@@ -1,19 +1,24 @@
 package analyzer
 
 import (
+	"bufio"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"os"
 	commands "server/commands"
 	"server/stores"
+	"server/structures"
 	"server/utils"
 	"strings"
+	"time"
 )
 
 func Analyzer(input string) (interface{}, error) {
 	tokens := strings.Fields(input)
 
 	if len(tokens) == 0 {
-		return nil, errors.New("no hay ningun comando")
+		return "", nil
 	}
 
 	switch strings.ToLower(tokens[0]) {
@@ -36,7 +41,9 @@ func Analyzer(input string) (interface{}, error) {
 				result += key + ", "
 			}
 		}
-		return fmt.Sprintf("particiones montadas: %s", result), nil
+		fmt.Println("Particiones montadas: ", result)
+
+		return fmt.Sprintf("Particiones montadas: %s", result), nil
 	case "mkfs":
 		return commands.ParseMkfs(tokens[1:])
 	case "cat":
@@ -48,9 +55,30 @@ func Analyzer(input string) (interface{}, error) {
 			return nil, errors.New("no hay sesion iniciada como para hacer un logout")
 		}
 		stores.LogedUser = ""
+		temp := stores.LogedIdPartition
 		stores.LogedIdPartition = ""
 		utils.LogedUserGroupID = 1
 		utils.LogedUserID = 1
+
+		sb, part, diskPath, err := stores.GetMountedPartitionSuperblock(temp)
+		if err != nil {
+			return nil, err
+		}
+		if sb.IsExt3() {
+			journalDirectory := &structures.Journal{
+				J_next: -1,
+				J_content: structures.Information{
+					I_operation: [10]byte{'l', 'o', 'g', 'o', 'u', 't'},
+					I_path:      [74]byte{},
+					I_content:   [64]byte{},
+					I_date:      float32(time.Now().Unix()),
+				},
+			}
+			err = sb.AddJournal(journalDirectory, diskPath, int32(part.Part_start+int32(binary.Size(structures.SuperBlock{}))))
+			if err != nil {
+				return nil, err
+			}
+		}
 		return "LOGOUT", nil
 	case "mkgrp":
 		return commands.ParseMkgrp(tokens[1:])
@@ -60,12 +88,29 @@ func Analyzer(input string) (interface{}, error) {
 		return commands.ParseMkusr(tokens[1:])
 	case "rmusr":
 		return commands.ParseRmusr(tokens[1:])
-	case "chgrp":
-		return commands.ParseChgrp(tokens[1:])
 	case "mkfile":
 		return commands.ParseMkfile(tokens[1:])
 	case "rep":
 		return commands.ParseRep(tokens[1:])
+	case "unmount":
+		return commands.ParseUnmount(tokens[1:])
+	case "find":
+		return commands.ParseFind(tokens[1:])
+	case "execute":
+		return ParseExecute(tokens[1:])
+	case "pause":
+		scanner := bufio.NewScanner(os.Stdin)
+		for {
+			fmt.Print("Apache ENTER para continuar: ")
+			if !scanner.Scan() {
+				break
+			}
+			input := scanner.Text()
+			if input == "" {
+				break
+			}
+		}
+		return "PAUSE", nil
 	default:
 		return nil, fmt.Errorf("comando desconocido: %v", tokens[0])
 	}
